92293ad243549051c838f4a5778be9665ece5675
Julius Alphonso <JadeMaveric@users.noreply.github.com>
2019-04-02 17:44:28 +0530

    rename variables

--- src/helper.js
+++ src/helper.js
@@ -1,16 +1,15 @@
 /**
  * Returns the nodes which have the shortest distance between the 2 components
- * @param {component} componentA 
- * @param {compenent} componentB 
- * @returns array with 2 nodes, representing the shortest connection between the 2 components
+ * @param {component} source 
+ * @param {compenent} target
  */
-function minDistance(componentA, componentB) {
-    var min = Math.pow(componentA[0].x - componentB[0].x, 2) + Math.pow(componentA[0].y - componentB[0].y, 2);
-    var ans = [componentA[0], componentB[0]]
-    for( i in componentA ) {
-        node1 = componentA[i];
-        for( j in componentB ) {
-            node2 = componentB[j];
+function minDistance(source, target) {
+    var min = Math.pow(source[0].x - target[0].x, 2) + Math.pow(source[0].y - target[0].y, 2);
+    var ans = [source[0], target[0]]
+    for( i in source ) {
+        node1 = source[i];
+        for( j in target ) {
+            node2 = target[j];
 
             // We're only comparing distances to see which is larger
             // No need to use computation heavy sqrt()
