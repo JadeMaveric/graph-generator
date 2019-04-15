f287ce4d631948e4c44bbc18b27649b4b6fe46b7
Julius Alphonso <julius@ieeegec.org>
2019-04-16 01:10:42 +0530

    Clean Code

--- src/app.js
+++ src/app.js
@@ -1,109 +1,67 @@
-var graphNumber = 0;
-
-var s = new sigma(
-  {
+var s = new sigma({
     renderer: {
-      container: document.getElementById('container'),
-      type: 'canvas'
+        container: document.getElementById('container'),
+        type: 'canvas'
     },
     settings: {
-      minArrowSize: 10
+        minArrowSize: 10
     }
-  }
-);
-
-sigma.classes.graph.addMethod('neighbors', function(nodeId) {
-  var k,
-  neighbors = {},
-  index = this.allNeighborsIndex[nodeId] || {};
-  
-  for (k in index)
-  neighbors[k] = this.nodesIndex[k];
-  
-  return neighbors;
 });
 
 function makeGraph() {
+    let numOfNodes = document.getElementById("num-nodes").value;
+    let numOfEdges = document.getElementById("num-edges").value;
+    let simulationTimeout = document.getElementById("sim-timeout").value;
+
     s.graph.clear();
     s.refresh();
 
-    let numOfNodes = document.getElementById("num-nodes").value;
-    let numOfEdges = document.getElementById("num-edges").value;
-
     let graph = {
-      nodes: [],
-      edges: []
+        nodes: [],
+        edges: []
     };
 
-    for (i = 0; i < numOfNodes; i++)
-      graph.nodes.push({ 
-        id:   i + graphNumber.toString(),
-        x: Math.random(),
-        y: Math.random(),
-        size: 1,
-        color: '#ec5148'
-      });
-
-    for (i = 0; i < numOfEdges; i++) {
-      let src  = '' + (Math.random() * numOfNodes | 0) + graphNumber.toString();
-      let dest = '' + (Math.random() * numOfNodes | 0) + graphNumber.toString();
-      graph.edges.push({ 
-        id: i + graphNumber.toString(), 
-        source: src, 
-        target: dest,
-        type: src==dest?"curvedArrow":"arrow"
-      });
+    for (i = 0; i < numOfNodes; i++) {
+        graph.nodes.push({ 
+            id:   i,
+            x: Math.random(),
+            y: Math.random(),
+            size: 0.3,
+            color: '#ec5148'
+        });
     }
 
-    s.graph.read(graph);
+    for (i = 0; i < numOfEdges; i++) {
+        let src  = '' + (Math.random() * numOfNodes | 0);
+        let dest = '' + (Math.random() * numOfNodes | 0);
+        let isCurved = src==dest;
+
+        graph.edges.forEach( e => {
+            if( (e.source == src) && (e.target == dest) ) {
+                isCurved = true;
+                e.type = "curvedArrow";
+                
+                let temp = src;
+                src = dest;
+                dest = temp;
+                
+            }
+        });
+
+        graph.edges.push({ 
+            id: i, 
+            source: src, 
+            target: dest,
+            type: isCurved?"curvedArrow":"arrow"
+        });
+    }
     
-    s.graph.nodes().forEach(function(n) {
-      n.originalColor = n.color;
-    });
-    s.graph.edges().forEach(function(e) {
-      e.originalColor = e.color;
-    });
-
-    graphNumber++;
-
+    s.graph.read(graph);
     s.refresh();
 
-    s.startForceAtlas2({
-      gravity: 2
-    });
-    window.setTimeout( ()=>s.killForceAtlas2(), 500 );
+    s.startForceAtlas2({ gravity: 2 });
+    window.setTimeout( ()=>s.killForceAtlas2(), simulationTimeout );
 
 }
-    s.bind('clickNode', function(e) {
-      var nodeId = e.data.node.id
-      var toKeep = s.graph.neighbors(nodeId);
-      toKeep[nodeId] = e.data.node;
-      
-      s.graph.nodes().forEach(function(n) {
-        if (toKeep[n.id])
-        n.color = n.originalColor;
-        else
-        n.color = '#eee';
-      });
-      
-      s.graph.edges().forEach(function(e) {
-        if (toKeep[e.source] && toKeep[e.target])
-        e.color = e.originalColor;
-        else
-        e.color = '#eee';
-    });
-    
-    s.refresh();
-  });
-  
-    s.bind('clickStage', function(e) {
-      s.graph.nodes().forEach(function(n) {
-      n.color = n.originalColor;
-    });
 
-    s.graph.edges().forEach(function(e) {
-      e.color = e.originalColor;
-    });
-
-    s.refresh();
-});
+makeGraph();
